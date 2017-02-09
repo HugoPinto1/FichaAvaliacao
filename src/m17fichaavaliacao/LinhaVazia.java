3c0543e5a999b0f89c1e9678ee2b02bed40b3c8c
a13226 <a13226@d1-08-03.dped.local>
2017-02-09 16:01:08 +0000

    Versão final

---
+++ src/m17fichaavaliacao/LinhaVazia.java
@@ -0,0 +1,22 @@
+package m17fichaavaliacao;
+
+/**
+ * Classe LinhaVazia, que herda da superclasse Exception,
+ * esta exceção vai ser utilizada, através de um if na classe Janela, para verificar
+ * a presença de um campo de texto vazio e mostrar a respetiva mensagem de erro.
+ * @author a13226 a15684
+ * @version 1.0 09.fev.2017
+ */
+
+public class LinhaVazia extends Exception{
+    
+    /**
+     * Construtor com parâmetros
+     * @param mensagem, a mensagem que vai aparecer, como erro, caso o utilizador deixe
+     * um campo de texto vazio.
+     */
+    
+    public LinhaVazia(String mensagem){
+        super(mensagem);
+    }
+}
